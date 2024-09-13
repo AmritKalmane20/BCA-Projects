7aae286cae533fcf901b6d47e96ec96a0cdc0936
AmritKalmane20 <amritakalmane@gmail.com>
2024-09-14 01:08:07 +0530

    first commit

---
+++ Semester1/p1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function to calculate nth Fibonacci number
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+
+    // Get the value of n from the user
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Call the fibonacci function and print the nth Fibonacci number
+    printf("The %dth Fibonacci number is: %d\n", n, fibonacci(n));
+
+    return 0;
+}
